1fb144fff879f0e5652bf56a4a5236a32f7a88e5
hemel18681 <hemel18103112@gmail.com>
2020-10-25 13:03:44 +0600

    work more

--- scripts.js
+++
@@ -1,7 +0,0 @@
-const selectElement = (s) => document.querySelector(s);
-selectElement('.open').addEventListener('click', () => {
-    selectElement('.nav-list').classList.add('active');
-});
-selectElement('.close').addEventListener('click', () => {
-    selectElement('.nav-list').classList.remove('active');
-});
