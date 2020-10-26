8de47ec38bcb24d7aa78b6c8e66809794a2212a8
hemel18681 <hemel18103112@gmail.com>
2020-10-26 22:27:38 +0600

    work done change if your wish

--- main.js
+++ main.js
@@ -3,4 +3,24 @@ $(document).ready(function() {
         $(this).toggleClass('open');
         $('.top-nav').toggleClass('open');
     });
+    $('.top-nav .nav-link').on('click', function() {
+        $('.menu-toggler').removeClass('open');
+        $('.top-nav').removeClass('open');
+    });
+    $('nav a[href*="#"]').on('click', function() {
+        $('html,body').animate({
+            scrollTop: $($(this).attr('href')).offset().top - 30
+        }, 2000);
+    });
+    $('#up').on('click', function() {
+        $('html,body').animate({
+            scrollTop: 0
+        }, 2000);
+    });
+
+    AOS.init({
+        easing: 'ease',
+        duration: 1800,
+        once: true
+    });
 });
