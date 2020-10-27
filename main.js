02c8bb2e83d41be37ccf0c4734a893515523bbcf
hemel18681 <hemel18103112@gmail.com>
2020-10-27 11:46:17 +0600

    work done, change according to wish

--- main.js
+++ main.js
@@ -17,10 +17,23 @@ $(document).ready(function() {
             scrollTop: 0
         }, 2000);
     });
-
     AOS.init({
         easing: 'ease',
         duration: 1800,
         once: true
     });
 });
+
+function boxMes() {
+    var name = document.getElementById('name').value;
+    var email = document.getElementById('email').value;
+    var service = document.getElementById('service').value;
+    var message = document.getElementById('message').value;
+    if (name.length != 0 && email.length != 0 && service.length != 0 && message.length != 0) {
+        swal({
+            title: "Thank You",
+            text: "Your Message has been delivered.",
+            icon: "success",
+        });
+    }
+}
